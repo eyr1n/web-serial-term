56c70734ceb0ff21b421f3fb6c463cf30fd321e9
Rin Iwai <rin@eyrin.jp>
2024-05-13 07:02:53 +0900

    use webgl and custom addon

--- src/XTerm.tsx
+++ src/XTerm.tsx
@@ -4,6 +4,8 @@ import type React from 'react';
 import { forwardRef, useEffect, useImperativeHandle, useRef } from 'react';
 
 import '@xterm/xterm/css/xterm.css';
+import { WebglAddon } from '@xterm/addon-webgl';
+import { StreamAddon } from './StreamAddon';
 
 export interface XTermProps extends React.HTMLAttributes<HTMLDivElement> {
   reader?: ReadableStreamDefaultReader<string | Uint8Array>;
@@ -19,64 +21,43 @@ export const XTerm = forwardRef<Terminal, XTermProps>(function XTerm(
 
   useImperativeHandle(ref, () => terminal.current, []);
 
-  // XTerm
   useEffect(() => {
     if (!container.current) {
       return;
     }
+    const webglAddon = new WebglAddon();
     const fitAddon = new FitAddon();
-    const observer = new ResizeObserver(() => {
-      fitAddon.fit();
-    });
+    terminal.current.loadAddon(webglAddon);
     terminal.current.loadAddon(fitAddon);
     terminal.current.open(container.current);
     fitAddon.fit();
+
+    const listener = webglAddon.onContextLoss(() => {
+      webglAddon.dispose();
+      terminal.current.loadAddon(webglAddon);
+    });
+    const observer = new ResizeObserver(() => {
+      fitAddon.fit();
+    });
     observer.observe(container.current);
+
     return () => {
       observer.disconnect();
+      listener.dispose();
+      webglAddon.dispose();
     };
   }, []);
 
-  // Reader
   useEffect(() => {
-    if (!reader) {
+    if (!container.current || !reader || !writer) {
       return;
     }
-    const controller = new AbortController();
-    abortable(reader.read(), controller.signal)
-      .then(function loop({ value, done }) {
-        if (done) {
-          reader.releaseLock();
-          return;
-        }
-        terminal.current?.write(value);
-        abortable(reader.read(), controller.signal)
-          .then(loop)
-          .catch(() => {});
-      })
-      .catch(() => {});
+    const streamAddon = new StreamAddon(reader, writer);
+    terminal.current.loadAddon(streamAddon);
     return () => {
-      controller.abort();
+      streamAddon.dispose();
     };
-  }, [reader]);
-
-  // Writer
-  useEffect(() => {
-    if (!writer) {
-      return;
-    }
-    const onData = terminal.current.onData((data) => writer.write(data));
-    return () => {
-      onData.dispose();
-    };
-  }, [writer]);
+  }, [reader, writer]);
 
   return <div ref={container} {...props} />;
 });
-
-function abortable<T>(promise: Promise<T>, signal: AbortSignal) {
-  return new Promise<T>((resolve, reject) => {
-    promise.then(resolve);
-    signal.addEventListener('abort', reject, { once: true });
-  });
-}
